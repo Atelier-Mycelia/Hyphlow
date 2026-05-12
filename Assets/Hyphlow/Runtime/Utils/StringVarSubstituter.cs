6299e6861b5892b31d45635c32794e7ff12aaf5a
CG-Tespy <gbcabreracgt@gmail.com>
2026-05-12 16:55:42 -0400

    Cleanups | Bugfix | New VarData | Updated readme and package.json
    Cleanups:
    - Removed unnecessary usings
    - Redundancy reduction
    - More expressive BlockClipboardEntry
    
    Bugfixes:
    - Some things such as the _targetButton AnyVariableData in ButtonClicked
    EventHandlers not getting pasted properly in the FcWindow
      - Seems that pasting ManagedReferences is more error-prone in Unity 2022.3 than it is
      in Unity 6
    
    New VarData:
    - DoubleData
      - For cases where stuff like audio systems need the extra precision

--- Assets/Hyphlow/Runtime/Utils/StringVarSubstituter.cs
+++ Assets/Hyphlow/Runtime/Utils/StringVarSubstituter.cs
@@ -1,4 +1,3 @@
-using AtMycelia.Collections;
 using AtMycelia.Hyphlow.Sys;
 using System;
 using System.Collections.Generic;
--- Assets/Hyphlow/Runtime/Utils/VariableRegistryService.cs
+++ Assets/Hyphlow/Runtime/Utils/VariableRegistryService.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using AtMycelia.Hyphlow.Sys;
 using UnityEngine;
-using AtMycelia.Collections;
 
 
 #if UNITY_EDITOR
